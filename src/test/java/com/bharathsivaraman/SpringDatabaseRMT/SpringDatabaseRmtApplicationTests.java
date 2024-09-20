96ac2eed6ae640f0981a2ea91ef0ccfc558b23d1
Bharath Sivaraman <bharathsivagiri2001@gmail.com>
2024-09-20 10:31:02 +0530

    reinitialized files

---
+++ src/test/java/com/bharathsivaraman/SpringDatabaseRMT/SpringDatabaseRmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bharathsivaraman.SpringDatabaseRMT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDatabaseRmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
